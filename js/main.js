d5c9da58b2cc965f71662779ef8ed6f4f214907a
Alexander Feigenbaum <tsamisaherb@gmail.com>
2017-04-25 08:09:56 -0400

    Added timer and score functionality
    added timer, score, broken clicking method

--- js/main.js
+++ js/main.js
@@ -56,6 +56,63 @@ var Main = function(game){
 	 
 	    //Set up some initial tiles and the score label
 	    me.initTiles();
+
+	    me.guessing = false;
+		me.currentWord = [];
+		me.correctWords = [];
+	 	
+		//on click down
+	 	me.game.input.onDown.add(function(){me.guessing = true;}, me);
+	 	//on click up
+		me.game.input.onUp.add(function(){me.guessing = false;}, me);
+
+		//Set the width and height for the tiles
+		me.tileWidth = 150;
+		me.tileHeight = 150;
+		 
+		//A buffer for how much of the tile activates a select
+		me.selectBuffer = me.tileWidth / 8;
+
+		//Keep track of the users score
+		me.score = 0;
+		me.scoreBuffer = 0;
+
+		me.createScore();
+
+		me.remainingTime = 6000;
+		me.fullTime = 6000;
+
+		me.createTimer();
+ 
+		me.gameTimer = game.time.events.loop(100, function(){
+		    me.updateTimer();
+		});
+	},
+
+	createTimer: function(){
+	 
+	    var me = this;
+	 
+	    me.timeBar = me.game.add.bitmapData(me.game.width, 50);
+	 
+	    //make white and have a blue background
+	    me.timeBar.ctx.rect(0, 0, me.game.width, 50);
+	    me.timeBar.ctx.fillStyle = '#ffffff';
+	    me.timeBar.ctx.fill();
+	 
+	    me.timeBar = me.game.add.sprite(0, 0, me.timeBar);
+	    me.timeBar.cropEnabled = true;
+	 
+	},
+	 
+	updateTimer: function(){
+	 
+	    var me = this;
+	 
+	    me.remainingTime -= 10;
+	 
+	    var cropRect = new Phaser.Rectangle(0, 0, (me.remainingTime / me.fullTime) * me.game.width, me.timeBar.height);
+	    me.timeBar.crop(cropRect);
 	 
 	},
 
@@ -128,9 +185,119 @@ var Main = function(game){
 	 
 	},
 
-	update: function() {
 
+	incrementScore: function(){
+ 
+    var me = this;
+ 
+    me.score += 1;   
+    me.scoreLabel.text = me.score;      
+ 
 	},
+	 
+	createScore: function(){
+	 
+	    var me = this;
+	    var scoreFont = "100px Arial";
+	 
+	    me.scoreLabel = me.game.add.text(me.game.world.centerX, me.topBuffer + 10 + me.tileGrid.length * me.tileHeight, "0", {font: scoreFont, fill: "#ffffff", stroke: "#535353", strokeThickness: 15}); 
+	    me.scoreLabel.anchor.setTo(0.5, 0);
+	    me.scoreLabel.align = 'center';
+	 
+	},
+
+	update: function() {
+    
+    var me = this;
+ 
+    if(me.guessing){
+ 
+        //Get the location of where the pointer is currently
+        var hoverX = me.game.input.x;
+        var hoverY = me.game.input.y;
+ 
+        //Figure out what position on the grid that translates to
+        var hoverPosX = Math.floor((hoverX - me.leftBuffer)/me.tileWidth);
+        var hoverPosY = Math.floor((hoverY - me.topBuffer)/me.tileHeight);
+ 
+        //Check that we are within the game bounds
+        if(hoverPosX >= 0 && hoverPosX < me.tileGrid[0].length && hoverPosY >= 0 && hoverPosY < me.tileGrid.length){
+ 
+            //Grab the tile being hovered over
+            var hoverTile = me.tileGrid[hoverPosX][hoverPosY];
+ 
+            //Figure out the bounds of the tile
+            var tileLeftPosition = me.leftBuffer + (hoverPosX * me.tileWidth);
+            var tileRightPosition = me.leftBuffer + (hoverPosX * me.tileWidth) + me.tileWidth;
+            var tileTopPosition = me.topBuffer + (hoverPosY * me.tileHeight);
+            var tileBottomPosition = me.topBuffer + (hoverPosY * me.tileHeight) + me.tileHeight;
+ 
+            //If the player is hovering over the tile set it to be active. The buffer is provided here so that the tile is only selected
+            //if the player is hovering near the center of the tile
+            if(!hoverTile.isActive && hoverX > tileLeftPosition + me.selectBuffer && hoverX < tileRightPosition - me.selectBuffer 
+                && hoverY > tileTopPosition + me.selectBuffer && hoverY < tileBottomPosition - me.selectBuffer){
+ 
+                //Set the tile to be active
+                hoverTile.isActive = true;
+ 
+                console.log(hoverTile.tileLetter);
+ 
+                //Push this tile into the current word that is being built
+                me.currentWord.push(hoverTile);
+            }
+ 
+        }
+ 
+    }
+    else {
+ 
+        if(me.currentWord.length > 0){
+ 
+            var guessedWord = '';
+ 
+            //Build a string out of all of the active tiles
+            for(var i = 0; i < me.currentWord.length; i++){
+                guessedWord += me.currentWord[i].tileLetter;
+                me.currentWord[i].isActive = false;
+            }
+ 
+            //Check to see if this word exists in our dictionary
+            if(me.game.cache.getText('dictionary').indexOf(' ' + guessedWord + ' ') > -1 && guessedWord.length > 1){
+
+				if(me.correctWords.indexOf(guessedWord) == -1){
+				 
+				    console.log("correct!");
+				 
+				    me.scoreBuffer += 10 * guessedWord.length;
+				 
+				    //Add this word to the already guessed word
+				    me.correctWords.push(guessedWord);
+				 
+					}
+ 
+                } 
+ 
+            } 
+            else {
+                console.log("incorrect!");
+            }
+ 
+            //Reset the current word
+            me.currentWord = [];
+ 
+        }
+
+	 	if(me.scoreBuffer > 0){
+	    me.incrementScore();
+	    me.scoreBuffer--;
+		}
+
+
+		if(me.remainingTime < 1){
+	    me.game.state.restart();
+		} 
+
+},
 
 	gameOver: function(){
 		this.game.state.start('GameOver');
